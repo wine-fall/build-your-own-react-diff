e46ff775a21df1284ffff03f21cb66131b4628ab
guozhiyi01 <guozhiyi01@baidu.com>
2022-11-02 11:07:54 +0800

    feat: add some utils

---
+++ utils/index.js
@@ -0,0 +1,43 @@
+import { createDom, Fiber, JSXElement } from "../src/react-diff.js";
+
+/**
+ * 
+ * @param {string[]} list 
+ * @returns {Fiber}
+ */
+export const createFiberList = (list) => {
+    const dummyHead = new Fiber('#', '#');
+    let p = dummyHead;
+    for (let i = 0; i < list.length; i++) {
+        const fiber = new Fiber(list[i], 'div', i, createDom('div'));
+        p.sibling = fiber;
+        p = p.sibling;
+    }
+    return dummyHead.sibling;
+};
+
+/**
+ * 
+ * @param {string[]} list 
+ * @returns {JSXElement[]}
+ */
+export const createJSXList = (list) => {
+    return list.map(key => new JSXElement(key, 'div'));
+};
+
+/**
+ * 
+ * @param {Fiber} fiber 
+ * @returns {any[]}
+ */
+export const convertFiberListToArray = (fiber) => {
+    const ret = [];
+    while (fiber !== null) {
+        ret.push({
+            key: fiber.key,
+            flags: fiber.flags
+        });
+        fiber = fiber.sibling;
+    }
+    return ret;
+};
