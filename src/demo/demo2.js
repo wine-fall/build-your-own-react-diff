31173359123ea306fe9f3bc02f9ca44306a4e893
guozhiyi01 <guozhiyi01@baidu.com>
2022-11-11 11:39:01 +0800

    feat: add demos

---
+++ src/demo/demo2.js
@@ -0,0 +1,19 @@
+import { convertFiberListToArray, createFiberList, createJSXList } from "../../utils/index.js";
+import { reconcileChildrenArray, Fiber} from "../react-diff.js";
+
+
+const list1 = ['a', 'b', 'c', 'd'];
+const list2 = ['a', null, 'c', 'd'];
+
+const returFiber = new Fiber('parent', 'div');
+const oldFiber = createFiberList(list1);
+const newChildren = createJSXList(list2);
+const newFiber = reconcileChildrenArray(returFiber, oldFiber, newChildren);
+
+
+console.log({
+    returFiberFlag: returFiber.flags,
+    returFiberDeletions: returFiber.deletions.map((fiber) => fiber.key),
+    oldEle: convertFiberListToArray(oldFiber),
+    newEle: convertFiberListToArray(newFiber)
+});
